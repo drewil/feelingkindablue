967988b37a3ef52c4abdab32bc09aa8e347bf04c
Dhruvil <shahd883@newschool.edu>
2024-02-11 14:38:38 -0500

    Add arena.js

---
+++ arena.js
@@ -0,0 +1,10 @@
+// This allows us to process/render the descriptions, which are in Markdown!
+// More about Markdown: https://en.wikipedia.org/wiki/Markdown
+let markdownIt = document.createElement('script')
+markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
+document.head.appendChild(markdownIt)
+
+// Okay, Are.na stuff!
+let channelSlug = 'feeling-kinda-blue' // The “slug” is just the end of the URL
+
+console.log('hi')
