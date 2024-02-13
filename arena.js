51fc8a4adff5f7696ffea2376905189d39704312
Dhruvil <shahd883@newschool.edu>
2024-02-13 00:06:20 -0500

    Trying JS

--- arena.js
+++ arena.js
@@ -6,3 +6,38 @@ document.head.appendChild(markdownIt)
 
 // Okay, Are.na stuff!
 let channelSlug = 'feeling-kinda-blue' // The “slug” is just the end of the URL
+
+// First, let’s lay out some *functions*, starting with our basic metadata:
+let placeChannelInfo = (data) => {
+	// Target some elements in your HTML:
+	let channelTitle = document.getElementById('channel-title')
+	// let channelDescription = document.getElementById('channel-description')
+	// let channelCount = document.getElementById('channel-count')
+	// let channelLink = document.getElementById('channel-link')
+
+	// Then set their content/attributes to our data:
+	channelTitle.innerHTML = data.title
+	// channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
+	// channelCount.innerHTML = data.length
+	// channelLink.href = `https://www.are.na/channel/${channelSlug}`
+}
+
+console.log(data)
+// Now that we have said what we can do, go get the data:
+fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
+	.then((response) => response.json()) // Return it as JSON data
+	.then((data) => { // Do stuff with the data
+		console.log(data) // Always good to check your response!
+		placeChannelInfo(data) // Pass the data to the first function
+
+		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+		data.contents.reverse().forEach((block) => {
+			// console.log(block) // The data for a single block
+			renderBlock(block) // Pass the single block data to the render function
+		})
+
+		// Also display the owner and collaborators:
+		let channelUsers = document.getElementById('channel-users') // Show them together
+		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
+		renderUser(data.user, channelUsers)
+	})
